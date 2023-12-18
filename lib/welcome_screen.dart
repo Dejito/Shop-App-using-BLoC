e7a0cddd1e541be26e3d282dcb11745ee27392c7
Deji <Deerealboy@gmail.com>
2023-12-18 15:43:31 +0100

    pageview added

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -20,46 +20,27 @@ class _WelcomeState extends State<Welcome> {
             children: [
               PageView(
                 children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        width: 345.w, height: 345.w,
-                        child: const Text(
-                            "Image One"
-                        ),
-                      ),
-                      Container(
-                        child: Text(
-                          "First See Learning!",
-                          style: TextStyle(
-                            fontSize: 24.sp,
-
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 375.w,
-                        padding: EdgeInsets.symmetric(horizontal: 30.w),
-                        child: Text(
-                          "And through desire, man having separateth himself intermingleth with all knowlege!",
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(.5),
-                            fontSize: 14.sp,
-
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 325.w,
-                        height: 50.h,
-                        decoration: BoxDecoration(
-                          color: Colors.redAccent,
-                          borderRadius: BorderRadius.all(Radius.circular(15.w))
-                        ),
-                      )
-                    ],
+                  _page(
+                    1,
+                    context,
+                    'next',
+                    "First it was fragrance",
+                    "And through desire, a man having separateth himself intermingleth with all knowlege!",
+                  ),
+                  _page(
+                    2,
+                    context,
+                    'next',
+                    "Connect with everyone",
+                    "Then it turned to fire, my worship is my weapon, this is how I win my battles",
+                  ),
+                  _page(
+                    3,
+                    context,
+                    'get started',
+                    "This is how I win",
+                    "And through desire, a man having separateth himself intermingleth with all knowlege!"
                   )
-
                 ],
               )
             ],
@@ -68,4 +49,62 @@ class _WelcomeState extends State<Welcome> {
       ),
     );
   }
+}
+
+Widget _page(int index, BuildContext context, String buttonName, String title,
+    String subtitle) {
+  return Column(
+    children: [
+      SizedBox(
+        width: 345.w,
+        height: 345.w,
+        child: const Text("Image One"),
+      ),
+      Container(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24.sp,
+          ),
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 12.h),
+        width: 375.w,
+        padding: EdgeInsets.symmetric(horizontal: 30.w),
+        child: Text(
+          subtitle,
+          style: TextStyle(
+            color: Colors.black.withOpacity(.5),
+            fontSize: 14.sp,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+        width: 325.w,
+        height: 50.h,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(15.w)),
+          // boxShadow: BoxShadow(
+          //   color: Colors.grey.withOpacity(0.1),
+          //   spreadRadius: 1,
+          //   blurRadius: 2,
+          //   offset: Offset(0, 1)
+          // ),
+        ),
+        child: Center(
+          child: Text(
+            buttonName,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.sp,
+            ),
+          ),
+        ),
+      )
+    ],
+  );
 }
