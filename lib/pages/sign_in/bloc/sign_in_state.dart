01a66b69c3e260bba22780e984b189752fbb3007
Deji <Deerealboy@gmail.com>
2023-12-22 18:31:54 +0100

    sign in bloc state implemented

--- lib/pages/sign_in/bloc/sign_in_state.dart
+++ lib/pages/sign_in/bloc/sign_in_state.dart
@@ -7,7 +7,7 @@ class SignInState {
 
   const SignInState({ this.email = "", this.password = ""});
 
-  SignInState copyWith({ String? email,  String? password}) {
+  SignInState copyWith({String? email, String? password}) {
     return SignInState(
       email:  email ?? this.email,
       password: password ?? this.password,
