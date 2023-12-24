cb0a305671be1178a1d64543af1f768e4202e694
Deji <Deerealboy@gmail.com>
2023-12-24 07:01:59 +0100

    ios firebase setup

--- lib/pages/sign_in/sign_in_controller.dart
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app_using_bloc/pages/sign_in/bloc/sign_in_bloc.dart';
@@ -9,17 +10,31 @@ class SignInController {
   SignInController(this.context);
 
 
-  void handleSignIn(String type) {
+  void handleSignIn(String type) async {
     try {
       if(type == "email") {
         final state = context.read<SignInBloc>().state;
-        if (state.email.isEmpty) {
+        final email = state.email;
+        final password = state.password;
+
+        if (email.isEmpty) {
           //
         }
-        if (state.email.isEmpty) {
+        if (password!.isEmpty) {
           //
         }
 
+        final credentials = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+        if (credentials.user == null) {
+
+        }
+        if (!credentials.user!.emailVerified) {
+
+        }
+        var user = credentials.user;
+        if (user != null) {
+
+        }
       }
     } catch (e) {
 
