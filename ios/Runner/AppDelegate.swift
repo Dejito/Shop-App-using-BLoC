f28ab89ad0b7414cedd545e83175ad17d7d95a4d
Deji <Deerealboy@gmail.com>
2023-12-25 04:44:11 +0100

    ios firebase setup working - but not thoroughly tested

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,38 @@
-import SwiftUI
-import FirebaseCore
+// import SwiftUI
+// import FirebaseCore
+//
+// class AppDelegate: NSObject, UIApplicationDelegate {
+//   func application(_ application: UIApplication,
+//                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//     FirebaseApp.configure()
+//     return true
+//   }
+// }
+//
+// @main
+// struct YourApp: App {
+//   // register app delegate for Firebase setup
+//   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//
+//   var body: some Scene {
+//     WindowGroup {
+//       NavigationView {
+//         ContentView()
+//       }
+//     }
+//   }
+// }
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
+import UIKit
+import Flutter
 
-@main
-struct YourApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentView()
-      }
-    }
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
